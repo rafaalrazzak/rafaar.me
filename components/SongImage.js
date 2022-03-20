616d78319e6aab993f6120fc6f90bbdd3fcf35b0
Arch663 <rafaalrazzak666@gmail.com>
2022-03-20 09:55:03 +0700

    ..

--- components/SongImage.js
+++ components/SongImage.js
@@ -1,4 +1,4 @@
 import Image from './Image'
-export default function SongImage({ height, width, alt, src }) {
-  return <Image src={src} alt={alt} width={width} height={height} className="rounded-full" />
+export default function SongImage({ alt, src, size }) {
+  return <Image src={src} alt={alt} width={size} height={size} className="rounded-full" />
 }
--- components/Track.js
+++ components/Track.js
@@ -4,19 +4,22 @@ export default function Track(track) {
   const blurImg = ({ src }) => {
     const img = { src }
     img.style.filter = 'blur(10px)'
+    img.style.objectFit = 'cover'
+    img.style = ' width=50 height=50'
   }
   return (
-    <div className="relative my-3 flex h-full w-full flex-col justify-center overflow-hidden rounded-lg border-b bg-white/30 shadow-sm filter backdrop-blur-md transition-colors duration-500 ease-linear hover:bg-spotify-green/10 hover:text-white dark:border-gray-600/50 dark:bg-gray-800 dark:hover:bg-gray-700 md:flex-row">
+    <div className="relative my-4 flex h-full w-full flex-col justify-center overflow-hidden rounded-lg border-b bg-white/30 shadow-sm filter backdrop-blur-md transition-colors duration-500 ease-linear hover:bg-spotify-green/10 hover:text-white dark:border-gray-600/50 dark:bg-gray-800 dark:hover:bg-gray-700 md:flex-row">
       <Link href={track.songUrl} className="w-full">
         <div className="flex h-full w-full justify-start ">
           <div className="flex xs:hidden">
             <Image
-              className="h-12 w-12 transform items-start justify-start overflow-hidden rounded-tl-lg bg-cover bg-center transition-all duration-500 ease-in-out hover:scale-[1.15] xs:absolute xs:hidden xs:rounded-full md:h-auto md:w-full md:rounded-l-lg "
+              className="transform items-start justify-start overflow-hidden rounded-tl-lg bg-cover bg-center transition-all duration-500 ease-in-out hover:scale-[1.15] xs:absolute xs:hidden xs:rounded-full md:h-auto md:w-full md:rounded-l-lg "
               src={track.songImage}
-              width="150"
-              height="150"
+              width="130"
+              height="130"
               alt="image-song"
               placeholder="blur"
+              layout="fixed"
               blurDataURL={blurImg}
             />
           </div>

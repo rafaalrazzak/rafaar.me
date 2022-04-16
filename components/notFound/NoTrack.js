bda2bc74c7a07a5539a5e3cb40ac46d4963de435
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-04-16 09:58:51 +0700

    Update NoTrack.js

--- components/notFound/NoTrack.js
+++ components/notFound/NoTrack.js
@@ -1,18 +1,19 @@
 import useTranslation from 'next-translate/useTranslation'
 export default function noTrack(){
-	  const { t } = useTranslation()
-	return(
-		    <div className="flex items-center justify-center w-full"> 
-					<div className="p-"> 
-						<div className="lg:gap-4 lg:flex"> 
-							<div className="flex flex-col items-center justify-center"> 
-								<h1 className="font-bold text-slate-600 dark:text-slate-200 text-6xl">404</h1> 
-								<p className="text-center text-gray-500 md:text-lg">
-									<span className="text-red-500">Oops! </span>
-									{t("404:noTrackParagraph")}</p> 
-							</div>
-          </div>
+  const { t } = useTranslation()
+    return(
+       <div className="flex items-center justify-center w-full"> 
+          <div className="p-4"> 
+	     <div className="lg:gap-4 lg:flex"> 
+                <div className="flex flex-col items-center justify-center"> 
+		  <h1 className="font-bold text-slate-600 dark:text-slate-200 text-6xl">404</h1> 
+		    <p className="text-center text-gray-500 md:text-lg">
+		      <span className="text-red-500">Oops!</span>
+			{t("404:noTrackParagraph")}
+                    </p> 
+		</div>
+            </div>
         </div>
       </div>
-	)
+    )
 }

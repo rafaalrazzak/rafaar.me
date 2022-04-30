5fb3bab56a6bda150accc2cfc65daea202cac287
Rafa Al Razzak <rafaalrazzak666@gmail.com>
2022-04-30 15:01:02 +0000

    .

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,6 @@ import '@/css/prism.css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-import { motion } from 'framer-motion'
-
 import { Globals } from '@react-spring/shared'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import RSS from '@/components/Rss'
@@ -22,22 +20,15 @@ export default function App({ Component, pageProps, router }) {
   })
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y:-20 }}
-      animate={{ opacity: 1, y:0 }}
-      transition={{ ease: 'easeOut', duration: 0.5 }}
-      key={router.route}
-    >
-      <ThemeProvider attribute="class" enableSystem={true}>
-        <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-        </Head>
-        {isDevelopment && isSocket && <ClientReload />}
-        <LayoutWrapper>
-          <Component {...pageProps} key={router.route} />
-        </LayoutWrapper>
-        <RSS />
-      </ThemeProvider>
-    </motion.div>
+    <ThemeProvider attribute="class" enableSystem={true}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      {isDevelopment && isSocket && <ClientReload />}
+      <LayoutWrapper>
+        <Component {...pageProps} key={router.route} />
+      </LayoutWrapper>
+      <RSS />
+    </ThemeProvider>
   )
 }

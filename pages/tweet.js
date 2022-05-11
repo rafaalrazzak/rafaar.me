efc27388d3dc88a0abb174d994d011c54756291a
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-05-11 17:08:09 +0700

    Delete tweet.js

--- pages/tweet.js
+++
@@ -1,8 +0,0 @@
-import { TwitterTimelineEmbed } from 'react-twitter-embed'
-export default function Tweet() {
-  return (
-    <div className="h-screen">
-      <TwitterTimelineEmbed autoHeight screenName="rafa_ar_id" sourceType="profile" />
-    </div>
-  )
-}

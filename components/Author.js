e8bdc7cc4ab2629fbe5e3a7ecb3c595186f8007d
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-05-12 15:38:07 +0700

    .

--- components/Author.js
+++ components/Author.js
@@ -1,23 +1,36 @@
-import useTranslation from 'next-translate/useTranslation'
-import { FaTwitter } from 'react-icons/fa'
-import Image from '@/components/Image'
-import Link from '@/components/Link'
-export default function Author({ detail }) {
-  const { t } = useTranslation()
+import useTranslation from "next-translate/useTranslation";
+import { FaTwitter } from "react-icons/fa";
+import Image from "@/components/Image";
+import Link from "@/components/Link";
+import { convertImage, toBase64 } from "@/lib/utils/imageBlur";
 
+export default function Author({ detail }) {
+  const { t } = useTranslation();
   return (
     <ul className="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-4">
       {detail.map((author) => (
-        <li className="flex items-center justify-center space-x-2" key={author.name}>
+        <li
+          className="flex items-center justify-center space-x-2"
+          key={author.name}
+        >
           <Image
             src={`https://res.cloudinary.com/raf-ar/image/upload/v1651370642/blog/avatars/${author.name.toLowerCase()}.jpg`}
             width="38px"
             height="38px"
             alt={author.name}
+            type="circle"
             className="h-10 w-10 rounded-full"
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(
+              convertImage(
+                `https://res.cloudinary.com/raf-ar/image/upload/v1651370642/blog/avatars/${author.name.toLowerCase()}.jpg`,
+                10,
+                10
+              )
+            )}`}
           />
           <dl className="whitespace-nowrap text-sm font-medium leading-5">
-            <dt className="sr-only">{t('common:name')}</dt>
+            <dt className="sr-only">{t("common:name")}</dt>
             <dd>
               <Link
                 className="text-gray-900 transition hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
@@ -42,5 +55,5 @@ export default function Author({ detail }) {
         </li>
       ))}
     </ul>
-  )
+  );
 }

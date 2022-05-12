fbd917f3eda9be5d7a6c88ceaca243c990a80533
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-05-12 15:09:52 +0700

    .

--- components/Spotify/Track/Track.js
+++ components/Spotify/Track/Track.js
@@ -2,7 +2,7 @@ import Image from "@/components/Image";
 import Link from "@/components/Link";
 import { convertImage, toBase64 } from "@/lib/utils/imageBlur";
 
-export default function Track({ title, image, alt, url  }) {
+export default function Track({ title, image, alt, url, artist  }) {
   return (
     <div className="group relative my-4 flex h-auto w-full flex-col justify-center overflow-hidden rounded-lg border-b bg-white/30 shadow-sm filter backdrop-blur-md transition-colors duration-500 ease-linear hover:bg-spotify-green/10 hover:text-white dark:border-gray-600/50 dark:bg-gray-800 dark:hover:bg-gray-700">
       <Link href={url}>
@@ -25,10 +25,10 @@ export default function Track({ title, image, alt, url  }) {
           </div>
           <div className="relative flex w-full flex-col p-4 leading-relaxed">
             <h5 className="text-md pb-2 font-bold tracking-tight text-gray-900 dark:text-white md:text-xl">
-              {track.title}
+              {title}
             </h5>
             <p className="md:text-md text-sm text-gray-700 dark:text-gray-400">
-              {track.artist}
+              {artist}
             </p>
           </div>
         </div>
